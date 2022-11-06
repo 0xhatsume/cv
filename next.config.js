6de6771860b0c026c200d243992944ac0e4d6264
0xhatsume <0xhatsume@gmail.com>
2022-11-06 15:27:58 +0800

    chore: initial boiler commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+
+
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
+  assetPrefix: isProd ? '/cv/' : '',
+  images: {
+    loader: 'akamai',
+    path: '/',
+  },
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './node_modules/flowbite/**/*.js',
+    './src/components/**/*.{ts,tsx,js,jsx}', 
+    './src/pages/**/*.{ts,tsx,js,jsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
