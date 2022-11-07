ee26179dcf3020542328733eb7c33b1d1cdc129f
0xhatsume <0xhatsume@gmail.com>
2022-11-07 09:43:30 +0800

    bug: fix bug in image url prefix error

--- src/config/constants/deploy.ts
+++ src/config/constants/deploy.ts
@@ -1,2 +1,2 @@
 export const isProd = process.env.NODE_ENV === 'production';
-export const gitAddressPrefix = 'https://0xhatsume.github.io/vaults-and-stuff';
+export const gitAddressPrefix = 'https://0xhatsume.github.io/cv';
