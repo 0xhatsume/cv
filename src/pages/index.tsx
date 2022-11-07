57b1b21e5b97c2ac898e5e6d65934f42417bf8ad
0xhatsume <0xhatsume@gmail.com>
2022-11-07 19:13:10 +0800

    feat: add temp projects section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import { SideBar } from '../components';
+import { isProd, gitAddressPrefix } from '../config/constants';
 
 const Home: NextPage=()=>{
   return (
@@ -30,12 +31,12 @@ const Home: NextPage=()=>{
 
             {/* About Section */}
             <div className='mt-7 mb-5'>
-              <div className='font-bold text-black text-lg my-2'>
+              <div className='font-bold text-black text-2xl my-2'>
                 About
               </div>
 
               <div className='pl-5'>
-                <ul className='list-disc'>
+                <ul className='list-disc text-base'>
                   <li>I really LOVE building things since young.</li>
                   <li>Data Scientist/ML Engineer for 7+ years. Active organizer of local DS meetups.</li>
                   <li>{`Crypto in 2017, active Crypto-Twitter Anon in 2021 :)`}</li>
@@ -45,6 +46,64 @@ const Home: NextPage=()=>{
             </div>
             
             <hr className='my-2 h-px bg-gray-300 drop-shadow border-0'/>
+
+            {/* Projects Section */}
+            <div className='mt-5'>
+              <div className='font-bold text-black text-2xl my-4'>
+                Projects <span className="font-normal text-base">(Crypto)</span>
+              </div>
+
+              {/* PlateDao */}
+              <div className='my-4 mt-5'>
+                <div className='font-bold mb-2'>PlateDAO</div>
+                <div className="flex flex-row justify-center items-center
+                space-x-1 px-2
+                w-full
+                ">
+                  <img className="w-1/3" src={`${isProd ? gitAddressPrefix : ''}/img/platedao1.PNG`}></img>
+                  <img className="w-1/3" src={`${isProd ? gitAddressPrefix : ''}/img/platedao2.PNG`}></img>
+                  <img className="w-1/3" src={`${isProd ? gitAddressPrefix : ''}/img/platedao3.PNG`}></img>
+                </div>
+                <ul className='list-disc text-base px-5'>
+                  <li>Index Fund Contract (fork/modified Pie DAO)</li>
+                  <li>
+                    <a className="text-blue-700 hover:text-blue-400"
+                      target={"_blank"} 
+                      href="https://d31pgfdb08zpg3.cloudfront.net/">
+                      [DEMO here]</a> 
+                      (request for password)
+                    </li>
+                </ul>
+              </div>
+
+              <div className='my-4'>
+                <div className='font-bold mb-2'>Dutch Auction Contract</div>
+                <ul className='list-disc text-base px-5'>
+                  <li>Coded, verified dutch auction contract for UMAMI token distribution plans.</li>
+                  </ul>
+              </div>
+              
+              <div className='my-4'>
+                <div className='font-bold mb-2'>Private Artist Creator Contract</div>
+                <ul className='list-disc text-base px-5'>
+                  <li>Designed/built private contract for Crypto Artist  
+                    <span><a className="ml-2 text-blue-700 hover:text-blue-400" href="https://twitter.com/AizawaShizuka" target={"_blank"}>
+                      [Aizawa Shizuka]</a>
+                      </span>
+                  </li>
+                  </ul>
+              </div>
+            </div>
+
+            <hr className='my-2 h-px bg-gray-300 drop-shadow border-0'/>
+
+            {/* Work Experiences */}
+            <div className='mt-5'>
+              <div className='font-bold text-black text-2xl my-2'>
+                Work Experiences <span className="font-normal text-base">(Request to reveal company name.)</span>
+              </div>
+
+            </div>
 
           </div>
         </div>
